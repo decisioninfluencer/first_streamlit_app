69cbdd636e2f29deb224736a9624d68fcfa132dd
decisioninfluencer <119137455+decisioninfluencer@users.noreply.github.com>
2022-11-26 17:19:36 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.headeer('Breakfast Menu')
-streamlit.text("Omega 3 & Blueberry Oatmeal')
-streamlit.tet("Kale, Spinach & ROcket Smoothie')
-streamlit.text("Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.tet('Kale, Spinach & ROcket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
