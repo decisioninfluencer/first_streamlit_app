48d540dca1f61cb6d8345bce56f157a2afa11e64
decisioninfluencer <119137455+decisioninfluencer@users.noreply.github.com>
2022-11-26 17:18:49 -0500

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.headeer('Breakfast Menu')
+streamlit.text("Omega 3 & Blueberry Oatmeal')
+streamlit.tet("Kale, Spinach & ROcket Smoothie')
+streamlit.text("Hard-Boiled Free-Range Egg')
+
