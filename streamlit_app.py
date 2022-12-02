1dd10131f817c980723685d1e040734d79b8a566
decisioninfluencer <119137455+decisioninfluencer@users.noreply.github.com>
2022-12-02 16:04:34 -0500

    Update streamlit_app.py
    add thanks line

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,5 @@ streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
 #allow the end user to add a fruit
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.header("Thanks for adding" + add_my_fruit)
