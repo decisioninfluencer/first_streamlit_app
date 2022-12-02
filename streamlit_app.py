3464b1362f407c5296507152e29150e4a97c182e
decisioninfluencer <119137455+decisioninfluencer@users.noreply.github.com>
2022-12-02 16:34:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,22 +39,9 @@ else:
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # output it the screen as a table
   streamlit.dataframe(fruityvice_normalized)
+  
 except URLError as e:
   streamlit.error()
   
-#import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
-
-#allow the end user to add a fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write("Thanks for adding " + add_my_fruit)
-
-#
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
